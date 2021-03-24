cde6349a3394b081a6565dc5d4eccc9c9be9a7f8
madzewski <80688483+madzewski@users.noreply.github.com>
2021-03-24 17:48:25 +0100

    Added zad5.py

--- lista_2/zad5.py
+++ lista_2/zad5.py
@@ -1,102 +1,86 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-import numpy as np
+import random
 import math
+import copy
+import numpy as np
 
-#to będzie do poprawy
+def e_dist(v, w):
+    return math.sqrt((v[0] - w[0]) ** 2 + (v[1] - w[1]) ** 2)
 
-N = 200  # N x K wymiary planszy
-K = 200
-D = 10  # odległość między rysowanymi punktami
-S = 45  # minimalna odleglosc między odwiertami
-R = 10  # liczba odwiertów
-
-G = nx.Graph()
-VxPoints = np.arange(0, N + 1, D)
-VyPoints = np.arange(0, K + 1, D)
-count_nodes = math.floor(len(VxPoints) * len(VyPoints))
-
-Gpos = {}
-x_index = 0
-y_index = 0
-for node in range(count_nodes):
-    G.add_node(node)
-    x = VxPoints[x_index]
-    y = VyPoints[y_index]
-    x_index += 1
-    if x_index == len(VxPoints):
-        x_index = 0
-        y_index += 1
-    Gpos[node] = [x, y]
-
-H = nx.Graph()
-
-for h in range(R):
-    H.add_node(h)
-
-nodesArr = []
-for node in H.nodes():
-    nodesArr.append(node)
-
-Hpos = {}
-temp = True
+def holes(N, K, D):
+    holes = {}
+    for d in range(int(K/D)+1):
+        for p in range(int(N/D)+1):
+            holes[(D*d,D*p)] = False 
+    return holes
 
 
-def drillsPositions():
-    temp = True
-    count_rows = 1
-    for node in H.nodes():
-        if node == 0:
-            x_pos, y_pos = 0, 0
-            Hpos[node] = [x_pos, y_pos]
-        else:
-            y_pos = Hpos[node - 1][1]
-            if temp:
-                x_pos = Hpos[node - 1][0] + S
-                while x_pos % D != 0:
-                    x_pos += 1
-                if x_pos > N:
-                    temp = False
-                    count_rows += 1
-                    x_pos = 150
-                    y_pos += S
-                    while y_pos % D != 0:
-                        y_pos += 1
+def plot_net(N, K, D):
+    for y in range(int(K/D)+1):
+        plt.plot([y*D,y*D],[0,N], 'k:')
+    for x in range(int(N/D)+1):
+        plt.plot([K,0],[x*D,x*D], 'k:') 
+    
+    
+
+def plot_holes(N, K, D, b):
+    for d in range(int(K/D)+1):
+        for p in range(int(N/D)+1):
+            if (D*d,D*p) in b:
+                if b[(D*d,D*p)] == True:
+                    plt.plot([D*d],[D*p], 'ro')
+                else:
+                    plt.plot([D*d],[D*p], 'ko')
             else:
-                x_pos = Hpos[node - 1][0] - S
-                while x_pos % D != 0:
-                    x_pos -= 1
-                if x_pos < 0:
-                    temp = True
-                    count_rows += 1
-                    x_pos = 0
-                    y_pos += S
-                    while y_pos % D != 0:
-                        y_pos += 1
-
-            if y_pos > K:
-                print("Błąd")
-            Hpos[node] = [x_pos, y_pos]
-    free_row = []
-    for currentRow in range(count_rows - 1):
-        yNodesArr = []
-        for yNodes in Hpos:
-            if Hpos[yNodes][1] not in yNodesArr:
-                yNodesArr.append(Hpos[yNodes][1])
-        free_row.append(yNodesArr[currentRow])
-        for node in reversed(nodesArr):
-            if Hpos[node][1] not in free_row:
-                Hpos[node][1] += D
-                if Hpos[node][1] > K:
-                    print('Za duzo odwiertów!')
-                    Hpos[node][1] -= D
-                    return
+                print("Brak wierzchołka")
+    plt.show()
 
 
-drillsPositions()
-print(Hpos)
-HposFinal = Hpos
+def boreholes(S,R,all_b):
+    not_used_b = copy.copy(all_b)
+    used_b = [(0,0)]
+    not_used_b.pop((0,0))
+    all_b[0,0] = True
+    for x in range(R-1):
+        if not_used_b:
+            h = max_dist(not_used_b.keys(), used_b, S)
+            if h == ():
+                break
+            used_b.append(h)
+            not_used_b.pop(h)
+            all_b[h] = True
+        else:
+            break
+    return all_b
 
-nx.draw(G, Gpos, node_size=100, with_labels=False, node_color='brown')
-nx.draw(H, HposFinal, node_size=50, with_labels=False, node_color='yellow')
-plt.show()
+
+def max_dist(not_used_b, used_b, S):
+    temp_max_dist = 0
+    temp_b = ()
+    for x in not_used_b:
+        temp_dist = []
+        control = False
+        for z in used_b:
+            if e_dist(z, x) < S:
+                control = True
+                break
+            else:
+                temp_dist.append(e_dist(z, x))
+        if control == True:
+            continue
+        temp_mean = np.min(temp_dist)
+        if temp_mean > temp_max_dist:
+            temp_max_dist = temp_mean
+            temp_b = x
+    return temp_b
+    
+
+N, K = (6,6)
+D = 2
+S = 1
+R = 10
+all_b = holes(N,K,D)
+b = boreholes(S, R, all_b)
+plot_net(N,K,D)
+plot_holes(N, K, D, b)
