751531092ecb26a7a7654481da8297909a7b2720
Michal Chylak <michauchylak@gmail.com>
2021-04-07 14:08:21 +0200

    miller-rabin test complete

--- lista_3/zad_3.py
+++ lista_3/zad_3.py
@@ -1,5 +1,6 @@
 import random
 
+
 def fermat_test(p):
     t_count = 1000
     counter = 0
@@ -14,6 +15,29 @@ def fermat_test(p):
     else:
         return "Liczba nie pierwsza", ratio
 
+def miller_rabin_test(p):
+    t_count = 100
+    d = p-1
+    s = 0
+    while d % 2 == 0:
+        s = s + 1
+        d = d / 2
+    for i in range(0,t_count):
+        a = random.randint(2,p-2)
+        x = pow(a,d) % p
+        if x == 1 or x == p - 1:
+            break
+        j = 1
+        while j < s and x != p - 1:
+            x = pow(x,2) % p
+            if x == 1:
+                return "NIE"
+            j = j + 1
+            if x != p - 1:
+                return "NIE"
+    return "TAK"
 
-p = 22469
-print(fermat_test(p))
+
+p = 151
+# print(fermat_test(p))
+print(miller_rabin_test(p))
