aba8156a421e324e8befd919875e4875e4189d6b
Michal Chylak <michauchylak@gmail.com>
2021-04-07 17:44:33 +0200

    zad_3 solved problems

--- lista_3/zad_3.py
+++ lista_3/zad_3.py
@@ -11,33 +11,37 @@ def fermat_test(p):
             counter +=1
     ratio = counter/t_count
     if ratio > 0.95:
-        return "Liczba pierwsza", ratio
+        return "Liczba pierwsza"
     else:
-        return "Liczba nie pierwsza", ratio
+        return "Liczba nie pierwsza"
 
-def miller_rabin_test(p):
-    t_count = 100
-    d = p-1
-    s = 0
-    while d % 2 == 0:
-        s = s + 1
-        d = d / 2
-    for i in range(0,t_count):
-        a = random.randint(2,p-2)
-        x = pow(a,d) % p
+
+def miller_rabin(p):
+    if p == 2 or p == 3:
+        return "Liczba pierwsza"
+    if p % 2 == 0:
+        return "Liczba nie pierwsza"
+
+    r, s = 0, p - 1
+    t_count = 1000
+
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+    for z in range(t_count):
+        a = random.randrange(2, p - 1)
+        x = pow(a, s, p)
         if x == 1 or x == p - 1:
-            break
-        j = 1
-        while j < s and x != p - 1:
-            x = pow(x,2) % p
-            if x == 1:
-                return "NIE"
-            j = j + 1
-            if x != p - 1:
-                return "NIE"
-    return "TAK"
+            continue
+        for z in range(r - 1):
+            x = pow(x, 2, p)
+            if x == p - 1:
+                break
+        else:
+            return "Liczba nie pierwsza"
+    return "Liczba pierwsza"
 
 
-p = 151
-# print(fermat_test(p))
-print(miller_rabin_test(p))
+p = 15645
+print(fermat_test(p))
+print(miller_rabin(p))
