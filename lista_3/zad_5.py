2efda2ca3c03a29af214d8622e5bcae34e15dbf3
Michal Chylak <michauchylak@gmail.com>
2021-04-08 10:56:35 +0200

    von Hann added, max ferq deleted, np.pi deleted

--- lista_3/zad_5.py
+++ lista_3/zad_5.py
@@ -1,30 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.rcParams['figure.figsize'] = [18,6]
-plt.rcParams.update({'font.size': 15})
+plt.rcParams["figure.figsize"] = [18, 6]
+plt.rcParams.update({"font.size": 15})
 
-def signal_create():    # tworzenie sygnału wraz ze sztucznym szumem
-    f = np.sin(2*np.pi*50*T) + np.sin(2*np.pi*25*T) + np.sin(2*np.pi*100*T)
-    f_clean = f
-    f = f + np.random.randn(len(T)) + np.hanning(len(T))  # dodanie okna czasowego von Hanna do standardowego szumu
-    
-    return f_clean,f
 
-def f_fourier_transform(): # zastosowanie szybkiej transformaty Fouriera (FFT)
+def signal_create():  # tworzenie sygnału wraz ze sztucznym szumem
+    f = np.sin(50 * T) + np.sin(25 * T) + np.sin(100 * T)
+    f_clean = f * np.hanning(len(T * 0.01))
+    f = (f * np.hanning(len(T))) + np.random.randn(len(T))  # dodanie okna czasowego von Hanna do sygnału
+    return f_clean, f
+
+
+def f_fourier_transform():  # zastosowanie szybkiej transformaty Fouriera (FFT)
     n = len(T)
     f = signal_create()[1]
-    fhat = np.fft.fft(f,n)
+    fhat = np.fft.fft(f, n)
     PSD = fhat * np.conj(fhat) / n
-    frequency = (1/(DT*n)) * np.arange(n)
-    L = np.arange(1,np.floor(n/2),dtype='int')
+    frequency = (1 / (DT * n)) * np.arange(n)
+    L = np.arange(1, np.floor(n / 2), dtype="int")
 
     return frequency, L, PSD, fhat
 
 
-def noise_filter():     # odfiltrowanie szumów
+def noise_filter():  # odfiltrowanie szumów
     PSD, fhat = f_fourier_transform()[2:4]
-    indices = PSD > 100
+    indices = PSD > 10
+    # for i in range(1000):
+    #     if indices[i] == True:
+    #         z = i
+    # indices[z] = False
     PSDClean = PSD * indices
     fhat = indices * fhat
     inv_filtered = np.fft.ifft(fhat)
@@ -32,37 +37,37 @@ def noise_filter():     # odfiltrowanie szumów
     return inv_filtered, PSDClean
 
 
-def ploting(): # generowanie wykresów
+def ploting():  # generowanie wykresów
 
-    # pobranie danych do wygenerowania wykresów 
+    # pobranie danych do wygenerowania wykresów
 
-    f_clean,f = signal_create()
+    f_clean, f = signal_create()
     frequency, L, PSD = f_fourier_transform()[0:3]
     inv_filtered, PSDClean = noise_filter()
 
-    fig,axs = plt.subplots(3,1)
+    fig, axs = plt.subplots(3, 1)
 
     # wykres sygnału z sztucznie wygenerowanym szumem
 
     plt.sca(axs[0])
-    plt.plot(T,f,color='c',label='noise')
-    plt.plot(T,f_clean, color='r',label='clean')
-    plt.xlim(T[0],T[-1])
+    plt.plot(T, f, color="c", label="noise")
+    plt.plot(T, f_clean, color="r", label="clean")
+    plt.xlim(T[0], T[-1])
     plt.legend(loc=1)
 
     # wykres odfiltrowanego sygnału
 
     plt.sca(axs[1])
-    plt.plot(T,inv_filtered,color='r',linewidth=2,label='filtered')
-    plt.xlim(T[0],T[-1])
+    plt.plot(T, inv_filtered, color="r", linewidth=2, label="filtered")
+    plt.xlim(T[0], T[-1])
     plt.legend(loc=1)
-    
+
     # wykres odfiltrowanych częstotliwości
 
     plt.sca(axs[2])
-    plt.plot(frequency[L],PSD[L],color='c',linewidth=2,label='noise')
-    plt.plot(frequency[L],PSDClean[L],color='r',label='filtered')
-    plt.xlim(frequency[L[0]],frequency[L[-1]])
+    plt.plot(frequency[L], PSD[L], color="c", linewidth=2, label="noise")
+    plt.plot(frequency[L], PSDClean[L], color="r", label="filtered")
+    plt.xlim(frequency[L[0]], frequency[L[-1]])
     plt.legend(loc=1)
     plt.show()
 
@@ -70,7 +75,7 @@ def ploting(): # generowanie wykresów
 # stałe używane do wykonania zadania
 
 DT = 0.001
-T = np.arange(0,1,DT)
+T = np.arange(0, 1, DT)
 
 # uruchomienie programu
 
