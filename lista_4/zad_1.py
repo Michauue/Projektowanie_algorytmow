09789daf59c2ae6b4fb97601cb9ae5fc6d219a83
Michal Chylak <michauchylak@gmail.com>
2021-04-14 18:31:35 +0200

    zad_1 final version

--- lista_4/zad_1.py
+++ lista_4/zad_1.py
@@ -2,28 +2,41 @@ import random
 import pickle
 
 
-
-class robots():
+class robots:
     def __init__(self, par=None):
         if par is None:
-            self.database = {'id': [], 'type': [], 'mass': [], 'range': [], 'resolution': []}
+            self.database = {
+                "id": [],
+                "type": [],
+                "mass": [],
+                "range": [],
+                "resolution": [],
+            }
         else:
-            self.database = {'id': [par[0]], 'type': [par[1]], 'mass': [par[2]], 'range': [par[3]], 'resolution': [par[4]]}
+            self.database = {
+                "id": [par[0]],
+                "type": [par[1]],
+                "mass": [par[2]],
+                "range": [par[3]],
+                "resolution": [par[4]],
+            }
 
     def add_robot(self, par):
-        self.database['id'].append(par[0])
-        self.database['type'].append(par[1])
-        self.database['mass'].append(par[2])
-        self.database['range'].append(par[3])
-        self.database['resolution'].append(par[4])
+        self.database["id"].append(par[0])
+        self.database["type"].append(par[1])
+        self.database["mass"].append(par[2])
+        self.database["range"].append(par[3])
+        self.database["resolution"].append(par[4])
 
     def generate_robot(self):
-        id = ''.join(random.choice('0123456789ABCDEFGHIJKLMNOPSTUWXYZ') for i in range(10))
-        type = random.choice(['AUV','AFV','AGV'])
-        mass = random.randint(50,2000)
-        ran = random.randint(1,1000)
-        resolution = random.randint(1,30)
-        return [id, type, mass, ran, resolution]
+        robot_id = "".join(
+            random.choice("0123456789ABCDEFGHIJKLMNOPSTUWXYZ") for i in range(10)
+        )
+        type = random.choice(["AUV", "AFV", "AGV"])
+        mass = random.randint(50, 2000)
+        ran = random.randint(1, 1000)
+        resolution = random.randint(1, 30)
+        return [robot_id, type, mass, ran, resolution]
 
     def generate_robots(self, M):
         desc = []
@@ -31,16 +44,18 @@ class robots():
             desc.append(self.generate_robot())
         return desc
 
-def linear_search(vector, category, value):
-    id = None
+
+def lineraSearch(vector, category, value):
+    robot_id = None
     for x in range(len(vector)):
-        if vector[x][category] in (value, int(value)) :
-            id = x
+        if vector[x][category] in (value, int(value)):
+            robot_id = x
             break
-    return id
-    
-def linear_search_vector(vector, search_value):
-    id = None
+    return robot_id
+
+
+def lineraSearchVector(vector, search_value):
+    robot_id = None
     counter = 0
     for x in range(len(vector)):
         for y in range(len(search_value)):
@@ -48,54 +63,64 @@ def linear_search_vector(vector, search_value):
                 continue
             counter += 1
         if counter == len(search_value):
-            id = x
+            robot_id = x
             break
         else:
             counter = 0
-    return id
+    return robot_id
 
-def adv_linear_search_vector(vector, search_value):
+
+def advancedLineraSearchVector(vector, search_value):
     c = 0
     for y in range(len(search_value)):
         if type(search_value[y]) == list:
             c += 1
             for z in range(len(search_value[y])):
-                id =adv_linear_search_vector(vector, vector_merge(search_value, z, y))
-                if id != None:
-                    return id
-    if c == 0 and limit_check(search_value):
-        id = linear_search_vector(vector, search_value)
-        return id
-     
+                robot_id = advancedLineraSearchVector(vector, vectorMerge(search_value, z, y))
+                if robot_id != None:
+                    return robot_id
+    if c == 0 and limitCheck(search_value):
+        robot_id = lineraSearchVector(vector, search_value)
+        return robot_id
 
 
-def limit_check(search_value):
-    limits = {1: [None,'AUV','AFV','AGV'], 2: [50, 2000], 3: [1,1000], 4:[1,30]}
+def limitCheck(search_value):
+    limits = {1: [None, "AUV", "AFV", "AGV"], 2: [50, 2000], 3: [1, 1000], 4: [1, 30]}
     if search_value[1] in limits[1]:
-        if search_value[2] in range(limits[2][0],limits[2][1]) or search_value[2] == None:
-            if search_value[3] in range(limits[3][0],limits[3][1])or search_value[3] == None:
-                if search_value[4] in range(limits[4][0],limits[4][1])or search_value[4] == None:
+        if (
+            search_value[2] in range(limits[2][0], limits[2][1])
+            or search_value[2] == None
+        ):
+            if (
+                search_value[3] in range(limits[3][0], limits[3][1])
+                or search_value[3] == None
+            ):
+                if (
+                    search_value[4] in range(limits[4][0], limits[4][1])
+                    or search_value[4] == None
+                ):
                     return True
-    print("Błędne dane")
+    print("Błędne dane", search_value)
     return False
 
-def vector_merge(search_value, z, y):
-    return search_value[:y] + [search_value[y][z]] + search_value[y+1:]
+
+def vectorMerge(search_value, z, y):
+    return search_value[:y] + [search_value[y][z]] + search_value[y + 1 :]
 
 
 random.seed(254279)
 robots_database = robots()
-vector = robots_database.generate_robots(100000)
+vector = robots_database.generate_robots(1000000)
 
 
-category = int(input("Podaj kategorię: 0.Id 1.Typ 2.Masa 3.Zasięg 4.Rozdzielczość "))
-value = input("Podaj wartość: ")
-print(linear_search(vector, category, value))
+# category = int(input("Podaj kategorię: 0.robot_id 1.Typ 2.Masa 3.Zasięg 4.Rozdzielczość "))
+# value = input("Podaj wartość: ")
+# print(lineraSearch(vector, category, value))
 
-# print(linear_search_vector(vector, [None, 'AFV', None, 173, 13]))
-# print(adv_linear_search_vector(vector, [None, None, [550,72,1374,1250,715], [72,578,890,123,364], None]))
-# 
+# print(lineraSearchVector(vector, [None, 'AFV', None, 420, 13]))
 
-
-
-
+x = advancedLineraSearchVector(vector, [None, None, [22422,221373,221250,42220,420], [67222,12223,32264,42220,420], None])
+if x == None:
+    print("ERROR, brak robocika")
+else:
+    print(x, vector[x])
