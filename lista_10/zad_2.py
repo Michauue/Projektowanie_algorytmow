7a2c950dc24b734e04dd34f4842718b8d149898e
Michal Chylak <michauchylak@gmail.com>
2021-06-01 22:08:31 +0200

    greedy knapsack problem

--- lista_10/zad_2.py
+++ lista_10/zad_2.py
@@ -1,20 +1,42 @@
 import numpy as np
 
 
-def knapsack(items, weight):
+def knapsackProblem(items, weight):
     n = len(items)
-    cache = np.zeros((n + 1, weight + 1))
+    temp = np.zeros((n + 1, weight + 1))
 
     for i in range(1, n + 1):
         for w in range(weight + 1):
             item_weight, item_value = items[i - 1]
             if item_weight > w:
-                cache[i, w] = cache[i - 1, w]
+                temp[i, w] = temp[i - 1, w]
             else:
-                cache[i, w] = max(cache[i - 1, w], cache[i - 1, w - item_weight] + item_value)
-    return cache[n, weight]
+                temp[i, w] = max(temp[i - 1, w], temp[i - 1, w - item_weight] + item_value)
+    print(temp)
+    return print('Programowanie dynamiczne:',temp[n, weight])
 
-#        lampa    cormen    blender  koń        tytan    ozdoba   krzeslo  odkurzacz
-items = [(2, 40), (2, 160), (3, 70), (15, 300), (1, 70), (4, 25), (5, 25), (6, 180)]
+def knapsackProblemGreedy(items, weight):
+    n = len(items)
+    result = 0
+    items = sorted(items, key=lambda item: item[1])
+    for i in range(1,n+1):
+        item_weight, item_value = items[-i]
+        if item_weight <= weight:
+            result = result + item_value
+            weight = weight - item_weight
+    return print('Podejście zachłanne:',result)
 
-print(knapsack(items, 15))
+
+#    (weight, value)
+items = [(2, 40),
+        (2, 160),
+        (3, 70),
+        (8, 230),
+        (1, 70),
+        (4, 25),
+        (5, 25),
+        (6, 180)]
+
+
+knapsackProblem(items, 15)
+knapsackProblemGreedy(items, 15)
